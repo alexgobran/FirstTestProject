7bf26ea309b879a10a4aca50a728e285c18e1561
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-08-28 09:07:20 -0400

    first commit

---
+++ FirstTestProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FirstTestProject {
+    class Program {
+        static void Main(string[] args)
+            {
+            Console.WriteLine("Hello Young World!");
+            string Developer = "Alex";
+            string LastName = "";
+            LastName = "Gobran";
+
+
+            Console.WriteLine(Developer + " " + LastName);
+            Console.WriteLine("Address");
+        
+         
+            string City = "Cincinnati";
+            string State = "OH";
+            if (City == "Cincinnati");
+            {
+                Console.WriteLine("Cincinnati");
+                Console.WriteLine(State);
+            }
+            string zip= "34232";
+            Console.WriteLine(zip);
+           
+
+        }
+    }
+}
